8a15409d3551a03d44e808af4fb4a344a64cbdba
Martin <cedmarth@bcd074432761.ant.amazon.com>
2022-07-20 09:56:05 -0700

    Started RPG Project, Created src and test dir, Created main class and started Character class

---
+++ src/com/huntersadventure/model/Character.java
@@ -0,0 +1,18 @@
+package com.huntersadventure.model;
+
+public class Character {
+    String name;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
