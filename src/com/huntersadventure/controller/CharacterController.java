364f3e68e8d1d53577c60bce81e10e0553cc7bde
Franco-H <francohui@berkeley.edu>
2022-07-22 14:46:53 -0700

    Command functionality created, AB#109

--- src/com/huntersadventure/controller/CharacterController.java
+++ src/com/huntersadventure/controller/CharacterController.java
@@ -1,6 +1,7 @@
 package com.huntersadventure.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.huntersadventure.client.GameLogic;
 import com.huntersadventure.jsonparser.Json;
 import com.huntersadventure.model.Character;
 
@@ -8,6 +9,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -18,6 +21,19 @@ public class CharacterController {
 
     static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
+    List<String> commands = new ArrayList<>(Arrays.asList(
+            "look", "help", "quit"));
+
+    List<String> preparatoryCommands = new ArrayList<>(Arrays.asList(
+            "get", "go", "use"));
+
+    List<String> direction = new ArrayList<>(Arrays.asList(
+            "north", "south", "west", "east"));
+
+    // TODO: Retrieve items from JSON file and store in a list.
+    // Items in the room or from NPCs
+    List<String> items = new ArrayList<>(Arrays.asList("Item", "Item", "Item"));
+
     public void startGame() throws IOException {
         System.out.println("Welcome to the game!");
         // run help() to get a list of commands
@@ -26,14 +42,13 @@ public class CharacterController {
         String output;
 
         // Prompt for input until the user enters "quit". If the user enters quit, the game will exit.
-        System.out.println("Enter a command below.");
-        while (!(input = in.readLine()).equals("quit")) {
+        do {
             System.out.println("Enter a command below.");
             System.out.println(">");
             input = in.readLine();
             output = runCommand(input);
             System.out.println(output);
-        }
+        } while (!(input = in.readLine()).equals("quit"));
     }
 
     private void help() {
@@ -42,18 +57,112 @@ public class CharacterController {
         System.out.println("look - Read the description of the current room and the items available");
         System.out.println("get [item] - pick up the specified item");
         System.out.println("help - display commands available");
-        System.out.println("quit - exit the game");
+        System.out.println("quit - exit the game and return to menu");
         System.out.println("-----------------------------------------------------");
     }
 
-    public String runCommand(String inputstr) {
-        return "not yet implemented";
+    public String runCommand(String inputString) throws IOException {
+        List<String> userInputs;
+        String output = "ok";
+        String lowerCaseInput = inputString.trim().toLowerCase();
+        if (lowerCaseInput.equals("")) {
+            output = "Please enter a command.";
+        } else {
+            userInputs = userInputs(lowerCaseInput);
+            output = parseCommand(userInputs);
+        }
+        return output;
     }
 
-    private String parseCommand(List<String> wordlist) {
-        return "not yet implemented";
+    private static List<String> userInputs(String lowerCaseInput) {
+        String delims = "[ \t,.:;?!\"']+";
+        String[] words = lowerCaseInput.split(delims);
+
+        List<String> strlist = new ArrayList<>(Arrays.asList(words));
+        return strlist;
     }
 
+    private String parseCommand(List<String> wordlist) throws IOException {
+        String message;
+        if (wordlist.size () == 1) {
+            message = processSingleCommand(wordlist);
+        } else if (wordlist .size () == 2) {
+            message = processTwoCommand(wordlist);
+        } else {
+            message = "Invalid command.";
+        }
+        return message;
+    }
+
+    private String processSingleCommand(List<String> wordlist) throws IOException {
+        String commandOne;
+        String message = "";
+
+        commandOne = wordlist.get(0);
+        if (!commands.contains(commandOne)) {
+            message = commandOne + " is not a known verb! ";
+        } else {
+            switch (commandOne) {
+                case "help":
+                    help();
+                    break;
+                case "quit":
+                    // Back to Main.java
+                    GameLogic.startPrompt();
+                    break;
+                case "look":
+                    // TODO: Read description of the room and items available.
+                    message = "look()";
+                    break;
+                default:
+                    message = commandOne + " (not yet implemented)";
+                    break;
+            }
+        }
+        return message;
+    }
+
+    private String processTwoCommand(List<String> wordlist) {
+        String commandOne;
+        String commandTwo;
+        String message = "";
+        commandOne = wordlist.get(0);
+        commandTwo = wordlist.get(1);
+        if (!preparatoryCommands.contains(commandOne)) {
+            message = commandOne + " is not a valid preparatory command.";
+        }
+        // TODO: Implement player's function to move between rooms
+        if (commandOne.equals("go")) {
+            if (commandTwo.equals("north")) {
+                return "You go north.";
+            } else if (commandTwo.equals("south")) {
+                return "You go south.";
+            } else if (commandTwo.equals("west")) {
+                return "You go west.";
+            } else if (commandTwo.equals("east")) {
+                return "You go east.";
+            } else {
+                message = "Invalid direction.";
+            }
+        }
+
+        // TODO: Implement player's function to get or use items
+        if (commandOne.equals("get")) {
+            if (items.contains(commandTwo)) {
+                return "You pick up the " + commandTwo + ".";
+            } else {
+                message = "There is no " + commandTwo + " here.";
+            }
+        } else if (commandOne.equals("use")) {
+            if (items.contains(commandTwo)) {
+                return "You use the " + commandTwo + ".";
+            } else {
+                message = "There is no " + commandTwo + " here.";
+            }
+        }
+        return message;
+    }
+}
 
 //        try {
 //            JsonNode gtNode = Json.parse(new File("lib/locations/guardtower.json"));
@@ -93,4 +202,3 @@ public class CharacterController {
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
-}
